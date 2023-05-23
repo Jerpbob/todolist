e0c70fdda356ff339d8772e291e40b4a90244f66
Jeru Balares <jerupaulbalares@gmail.com>
2023-05-23 11:30:34 -0700

    Added templates folder with html, and configured some code in views.py to connect a simple greeting variable to the greet variable in the html file

--- flask/core/views.py
+++ flask/core/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from core import app
+
 
 @app.route('/')
 def index():
-    return 'Hello'
+    greeting="Hello there"
+    return render_template('index.html', greet=greeting)
